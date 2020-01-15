d694ec69d77bb70358d868d67b3e4001def777b0
Ben Thornton <smenjas@users.noreply.github.com>
2020-01-14 17:00:54 -0800

    Add chapter 10.

---
+++ 10-k-nearest-neighbors.py
@@ -0,0 +1,14 @@
+k = 2 # How many nearest neighbors?
+dimensions = 2
+
+users = ['Priyanka', 'Justin', 'Morpheus']
+
+genres = ['comedy', 'action', 'drama', 'horror', 'romance']
+
+ratings = {}
+ratings{"Priyanka":
+    ('comedy': 3)
+    ('action': 3)
+    ('drama': 3)
+    ('horror': 3)
+    ('romance': 3)}
